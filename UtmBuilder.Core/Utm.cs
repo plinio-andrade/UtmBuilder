89445397e638e743f2840ddaa2c9671408ed4d47
Plinio Andrade <andrade-plinio@outlook.com>
2023-11-26 11:35:09 -0300

    value objects added

---
+++ UtmBuilder.Core/Utm.cs
@@ -0,0 +1,21 @@
+using UtmBuilder.Core.ValueObjects;
+
+namespace UtmBuilder.Core;
+
+public class Utm
+{
+    public Utm(Url url, Campaign campaign)
+    {
+        Url = url;
+        Campaign = campaign;
+    }
+    /// <summary>
+    /// URL (Website link)
+    /// </summary>
+    public Url Url { get; }
+
+    /// <summary>
+    /// Campaign details
+    /// </summary>
+    public Campaign Campaign { get; }
+}
