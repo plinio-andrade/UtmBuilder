d5196c6816c01c9a9df7efb835f6badfd00f842b
Plinio Andrade <andrade-plinio@outlook.com>
2023-11-26 17:45:45 -0300

    url tests added

---
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -0,0 +1,53 @@
+using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
+namespace UtmBuilder.Core.Tests.ValueObjects;
+
+[TestClass]
+public class UrlTests
+{
+    private const string InvalidUrl = "banana";
+    private const string ValidUrl = "https://balta.io";
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidUrlException))]
+    public void ShouldReturnExceptionWhenUrlIsInvalid()
+    {
+        new Url(InvalidUrl);
+    }
+
+    [TestMethod]
+    public void ShouldNotReturnExceptionWhenUrlIsValid()
+    {
+        new Url(ValidUrl);
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    [DataRow(" ", true)]
+    [DataRow("http", true)]
+    [DataRow(InvalidUrl, true)]
+    [DataRow(ValidUrl, false)]
+    public void TestUrl(
+        string link,
+        bool expectException)
+    {
+        if (expectException)
+        {
+            try
+            {
+                new Url(link);
+                Assert.Fail();
+            }
+            catch (InvalidUrlException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        else
+        {
+            new Url(link);
+            Assert.IsTrue(true);
+        }
+    }
+}
